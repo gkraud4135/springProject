bd1a3869c3c76ec48d183a1a89f3b8992683cc26
hm <gkraud4135@naver.com>
2025-12-02 22:08:16 +0900

    test page

--- src/main/java/com/deploy/springProject/TestController.java
+++ src/main/java/com/deploy/springProject/TestController.java
@@ -5,8 +5,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+    @GetMapping("/")
+    public String index(){
+        return "안뇽 🦛💕🐥";
+    }
+
     @GetMapping("/test")
-    public String test(){
+    public String test2(){
         return "Hello, world!";
     }
 }
